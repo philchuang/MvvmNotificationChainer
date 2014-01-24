4fd9e9f427487ea6bde48bd802ecedfae1eef033
Phil Chuang <dev@philchuang.com>
2014-01-24 01:43:59 -0500

    re-added typed manager wrapper

--- MvvmNotificationChainer/NotificationChainManagerWrapper.cs
+++ MvvmNotificationChainer/NotificationChainManagerWrapper.cs
@@ -2,50 +2,82 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace Com.PhilChuang.Utils.MvvmNotificationChainer
 {
-    public interface INotificationChainManagerWrapper
+    public class NotificationChainManagerWrapper<T> : INotificationChainManager
     {
-        Object Wrapped { get; }
-        NotificationChainManager Manager { get; }
-    }
+        public object ObservedObject { get { return Manager != null ? Manager.ObservedObject : null; } }
 
-    public class NotificationChainManagerWrapper<T> : INotificationChainManagerWrapper, IDisposable
-        where T : class
-    {
-        public Object Wrapped { get { return WrappedTyped; } }
-        public T WrappedTyped { get; private set; }
+        public T Wrapped { get { return Manager != null ? (T) Manager.ObservedObject : default(T); } }
+
+        public bool IsDisposed { get; private set; }
+
         public NotificationChainManager Manager { get; private set; }
 
-        public NotificationChainManagerWrapper (INotifyPropertyChanged notifyingObject)
+        public NotificationChainManagerWrapper ()
         {
-            notifyingObject.ThrowIfNull ("notifyingObject");
-
-            Initialize ((T) notifyingObject, h => notifyingObject.PropertyChanged += h, h => notifyingObject.PropertyChanged -= h);
-        }
-
-        public NotificationChainManagerWrapper (T notifyingObject, Action<PropertyChangedEventHandler> addEventAction, Action<PropertyChangedEventHandler> removeEventAction)
-        {
-            notifyingObject.ThrowIfNull ("notifyingObject");
-
-            Initialize (notifyingObject, addEventAction, removeEventAction);
-        }
-
-        private void Initialize (T notifyingObject, Action<PropertyChangedEventHandler> addEventAction, Action<PropertyChangedEventHandler> removeEventAction)
-        {
-            WrappedTyped = notifyingObject;
-            
             Manager = new NotificationChainManager();
-            Manager.SetDefaultNotifyingObject (notifyingObject, addEventAction, removeEventAction);
+        }
+
+        public NotificationChainManagerWrapper (INotifyPropertyChanged notifyingObject) : this ()
+        {
+            Observe (notifyingObject);
+        }
+
+        public NotificationChainManagerWrapper (
+            Object notifyingObject,
+            Action<PropertyChangedEventHandler> addEventAction,
+            Action<PropertyChangedEventHandler> removeEventAction) : this ()
+        {
+            Observe (notifyingObject, addEventAction, removeEventAction);
         }
 
         public void Dispose ()
         {
-            Manager.Dispose();
+            if (IsDisposed) return;
+
+            Manager.Dispose ();
             Manager = null;
-            WrappedTyped = null;
+
+            IsDisposed = true;
         }
+
+        public void AddDefaultCall (Action onNotifyingPropertyChanged)
+        { Manager.AddDefaultCall (onNotifyingPropertyChanged); }
+
+        public void AddDefaultCall (NotificationChainCallback onNotifyingPropertyChanged)
+        { Manager.AddDefaultCall (onNotifyingPropertyChanged); }
+
+        public NotificationChain CreateOrGet<T1> (Expression<Func<T1>> propGetter)
+        { return Manager.CreateOrGet (propGetter); }
+
+        public NotificationChain CreateOrGet ([CallerMemberName] string dependentPropertyName = null)
+        { return Manager.CreateOrGet (dependentPropertyName); }
+
+        public NotificationChain Get ([CallerMemberName] string dependentPropertyName = null)
+        { return Manager.Get (dependentPropertyName); }
+
+        public void Clear ([CallerMemberName] string dependentPropertyName = null)
+        { Manager.Clear (dependentPropertyName); }
+
+        public void Observe<TInpc> (TInpc notifyingObject)
+            where TInpc : T, INotifyPropertyChanged
+        { Observe ((INotifyPropertyChanged) notifyingObject); }
+
+        public void Observe (INotifyPropertyChanged notifyingObject)
+        { Manager.Observe (notifyingObject); }
+
+        public void Observe (object notifyingObject, Action<PropertyChangedEventHandler> addEventAction, Action<PropertyChangedEventHandler> removeEventAction)
+        { Manager.Observe (notifyingObject, addEventAction, removeEventAction); }
+
+        public void StopObserving ()
+        { Manager.StopObserving(); }
+
+        public void Publish (object sender, PropertyChangedEventArgs args)
+        { Manager.Publish (sender, args); }
     }
 }
