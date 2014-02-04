a914ba638fe753fd137239d455ab349a5ae6c0b9
Phil Chuang <dev@philchuang.com>
2014-02-04 15:45:23 -0500

    added AttributeUsage to NotificationChainProperty

--- MvvmNotificationChainer/NotificationChainPropertyAttribute.cs
+++ MvvmNotificationChainer/NotificationChainPropertyAttribute.cs
@@ -6,6 +6,7 @@ using System.Text;
 
 namespace Com.PhilChuang.Utils.MvvmNotificationChainer
 {
+    [AttributeUsage (AttributeTargets.Property)]
     public class NotificationChainPropertyAttribute : Attribute
     {
         /// <summary>
